b36bc38ab97e3946d1e1686eef824173b6ca3922
Ranxin <rstli@ucdavis.edu>
2023-07-01 21:34:01 -0700

    change all const value from var to let

--- helloworld.swift
+++ helloworld.swift
@@ -1,9 +1,9 @@
 /* My first program in Swift 4 */
 
 func printVariableDemo(){
-    var myString = "Hello, World!"
+    let myString = "Hello, World!"
     print(myString)
-    var variable=42
+    let variable=42
     print("The value of variable is: \(variable)")
 }
 
@@ -15,10 +15,10 @@ func loopDemo(){
 }
 
 func stringDemo(){
-    var str="abcd"
+    let str="abcd"
     print("The length of str is \(str.count)")
     print("str equals to 'abce':\(str=="abce")")
-    var num_str="123"
+    let num_str="123"
     if let num = Int(num_str) {
         print("The num minus 10 is \(num - 10)")
     } else {
@@ -29,13 +29,13 @@ func stringDemo(){
     print("The second character is str is \(str[1])")
     */
     print("The second character is str is \(str[str.index(str.startIndex, offsetBy: 1)])")
-    var str_arr = Array(str.utf8CString)
+    let str_arr = Array(str.utf8CString)
     print("str_arr[1] is\(str_arr[1])")
 }
 
 func arrayDemo(){
     //var arrThreeRepeat0 = [Int](repeating: 0, count: 3)
-    var arrTenRepeat_1 = [Int](repeating: -1, count: 5)
+    let arrTenRepeat_1 = [Int](repeating: -1, count: 5)
     print("The element in arrTenRepeat_1 are:")
     for element in arrTenRepeat_1{
         print(element, terminator:"\t")
@@ -44,6 +44,9 @@ func arrayDemo(){
 
     var arr:[Int]=[3, 1, 4, 7]
     arr.append(9)
+    if arr[0] != 4{
+        print("The first element of arr does not equal to 4")
+    }
     for element in arr{
         print(element, terminator:"\t")
     }
@@ -52,7 +55,7 @@ func arrayDemo(){
 
 func matrixDemo(){
     print("matrix of 3*5 is ")
-    var matrix=[[Int]](repeating:[Int](repeating: 0, count:3), count:5)
+    let matrix=[[Int]](repeating:[Int](repeating: 0, count:3), count:5)
     for row in matrix{
         for value in row{
             print(value, terminator:"\t")
@@ -62,7 +65,7 @@ func matrixDemo(){
 }
 
 func setDemo(){
-    var primes:Set=[2,3,7, 5,7]
+    let primes:Set=[2,3,7, 5,7]
     print("Number of primes in 10:\(primes.count)")
     print("Whether 4 is a prime number:\(primes.contains(4))")
 }
@@ -73,9 +76,7 @@ func dictDemo(){
     myDict[20]="Twenty"
     myDict[30]="Thirty"
     print("Whether 40 exist in myDict: \(myDict.contains{ $0.key == 40})")
-    if arr[0] != 4{
-        print("The first element of arr does not equal to 4")
-    }
+    
     /*
     These code has some error and do not know how to debug
     var parenthesesDict: [Character: Character] = ["(":")", "[":"]", "{":"}"]
